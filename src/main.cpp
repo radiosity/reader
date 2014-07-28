9e0a06adf0699098946643c2ae3f0330f6019feb
Richard Martin <radiosity@gmail.com>
2014-07-28 19:33:23 +0100

    Clean the cruft out of main

--- src/main.cpp
+++ src/main.cpp
@@ -38,23 +38,10 @@ HeaderBar * header_bar;
 
 int main(int argc, char* argv[])
 {
-	/*Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
-
-	Gtk::Window window;
-	window.set_title("Drawing text example");
-
-	window.set_default_size(600,900);
-
-	BookReader area;
-	window.add(area);
-	area.show();
-
-	return app->run(window); */
 	
 	RefPtr<Application> app = Application::create(argc, argv, "org.rmarti.dnd");
 
 	MainWindow window;
 
-	// Shows the window and returns when it is closed.
 	return app->run(window);
 }
